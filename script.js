7f77eb7457d4d6b6cf679abb151ab267a1017ac4
SnipzzID <snipzzhealthysnack@gmail.com>
2025-06-21 13:41:26 +0700

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function sendWA() {
   const email = document.getElementById('email').value.trim();
   const alamat = document.getElementById('alamat').value.trim();
   const pesan = document.getElementById('pesan').value.trim();
-  const nomor = '6281234567890';
+  const nomor = '6285285673568';
   const text = `Halo SNIPZZ! Saya mau order:%0ANama: ${nama}%0AEmail: ${email}%0AAlamat: ${alamat}%0APesanan: ${pesan}`;
   window.open(`https://wa.me/${nomor}?text=${text}`, '_blank');
   return false;
